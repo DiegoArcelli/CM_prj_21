7cf74557bc0068c83be5443568c1380af94cf27b
Luca Moroni <terraformer.144@gmail.com>
2022-01-08 22:38:27 +0100

    try to solve projection theoretical error

--- src/projection.m
+++ src/projection.m
@@ -11,47 +11,56 @@ function [x_proj] = projection(a, l, u, b, y, eps)
 
     % use quick sort O(n^2) | avg O(n log n)
     break_points = union(mi_plus, mi_minus);
+    break_points = break_points(break_points>=0);
+    
+    disp(break_points)
     
     q_p = @(m) lagrangian_dual_prime(a_tilde, l_tilde, u_tilde, b_tilde, m);
     
+    fplot(q_p, [-100, 100])
+    
+    if isempty(break_points)
+        x_proj = min_x_mu(a_tilde, l_tilde, u_tilde, 0) + y;
+        return;
+    end
+    
     mu_l = 0;
     mu_u = 0;
-    g_u = q_p(break_points(end));
-    g_l = q_p(break_points(1));
-    
-    disp(q_p(break_points(1)))
-    
+    g_u = 0;
+    g_l = 0;
+        
     while ~isempty(break_points)
         j = ceil(length(break_points)/2);
         mu = break_points(j);
         
         if abs(q_p(mu)) < eps
-            x_proj = min_x_mu(l_tilde, u_tilde, mu) + y;
+            x_proj = min_x_mu(a_tilde, l_tilde, u_tilde, mu) + y;
             return;
         elseif q_p(mu) < 0
             mu_u = mu;
-            g_u = q_p(mu);
+            g_l = q_p(mu);
             break_points = break_points(break_points<mu);
         else
             mu_l = mu;
-            g_l = q_p(mu);
+            g_u = q_p(mu);
             break_points = break_points(break_points>mu);
         end
     end
     
     mu_star = mu_l + g_u*(mu_u - mu_l)/(g_u - g_l);
-    x_proj = min_x_mu(l_tilde, u_tilde, mu_star) + y;
+    x_proj = min_x_mu(a_tilde, l_tilde, u_tilde, mu_star) + y;
 end
 
 
-function [x_m] = min_x_mu(l, u, mu)
+function [x_m] = min_x_mu(a, l, u, mu)
     % docstring
-    x_m = median([l, u, repmat(-mu, size(u))], 2);
+    % x_m = median([l, u, repmat(-mu, size(u))], 2);
+    x_m = median([l, u, -(a*mu)/2], 2);
 end
 
 
 function [q_prime] = lagrangian_dual_prime(a, l, u, b, mu)
     % docstring
-    x_m = min_x_mu(l, u, mu);
+    x_m = min_x_mu(a, l, u, mu);
     q_prime = -b + a'*x_m;
 end
