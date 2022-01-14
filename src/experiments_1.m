c022ad6f3e9cbdbd484a4fda3a7635fad3697c7b
Luca Moroni <terraformer.144@gmail.com>
2022-01-14 15:22:03 +0100

    minor changes over experiments

--- src/experiments_1.m
+++ src/experiments_1.m
@@ -3,6 +3,8 @@
 % 2 dimensional problems
 n = 2;
 
+options = optimoptions('fmincon','Display','off');
+
 % -- random generated
 for i = 1:10
     [Q, q, l, u, a, b, x_start] = generate_problem(n, 5);
@@ -14,11 +16,10 @@ for i = 1:10
     toc();
 
     tic();
-    options = optimoptions('fmincon','Display','off');
     x_mega = fmincon(@(x) objective_function(Q, q, x), x_start, [-eye(n); eye(n); -a'], [-l; u; -b], [], [], [], [], [], options);
     toc();
 
-    disp(norm(x_star - x_mega));
+    disp(norm(x_star - x_mega)/norm(x_mega));
 end
 
 % 10 dimensional problems
@@ -35,11 +36,10 @@ for i = 1:10
     toc();
 
     tic();
-    options = optimoptions('fmincon','Display','off');
     x_mega = fmincon(@(x) objective_function(Q, q, x), x_start, [-eye(n); eye(n); -a'], [-l; u; -b], [], [], [], [], [], options);
     toc();
 
-    disp(norm(x_star - x_mega));
+    disp(norm(x_star - x_mega)/norm(x_mega));
 end
 
 % 100 dimensional problems
@@ -47,20 +47,17 @@ n = 100;
 
 % -- random generated
 for i = 1:10
-    
-    % !!! cannot able to generate the problem
-    [Q, q, l, u, a, b, x_start] = generate_problem(n, 100);
-    
+    [Q, q, l, u, a, b, x_start] = generate_problem(n, 5);
+
     L = max(eig(Q));
 
     tic();
-    [x_star, f_star, x_s, f_s, g_s] = KQP(Q, q, l, u, a, b , x_start, 10e-16, 10e-16, 1000, "fixed", 1/L, 1);
+    [x_star, f_star, x_s, f_s, g_s] = KQP(Q, q, l, u, a, b , x_start, 10e-16, 10e-16, 1000, "fixed", 1/L, 0);
     toc();
 
     tic();
-    options = optimoptions('fmincon','Display','off');
     x_mega = fmincon(@(x) objective_function(Q, q, x), x_start, [-eye(n); eye(n); -a'], [-l; u; -b], [], [], [], [], [], options);
     toc();
 
-    disp(norm(x_star - x_mega));
+    disp(norm(x_star - x_mega)/norm(x_mega));
 end
--- src/generate_problem.m
+++ src/generate_problem.m
@@ -1,16 +1,16 @@
 function [Q, q, l, u, a, b, x_start] = generate_problem(n, scale)
     % docstring
-    
+
     A = randn(n, n)*scale;
     Q = A'*A;
     q = randn(n, 1)*scale;
-    
+
     [l, u, a, b] = random_constraints(n, scale);
 
     while any(l > u) || a'*u < b
         [l, u, a, b] = random_constraints(n, scale);
     end
-            
+
     x_start = projection(l, u, a, b, randn(n, 1)*scale, 10e-10);
 end
 
@@ -18,8 +18,8 @@ end
 function [l, u, a, b] = random_constraints(n, scale)
     % docstring
 
-    l = rand(n, 1)*scale;
-    u = rand(n, 1)*scale;
+    l = rand(n, 1)*(scale/2);
+    u = l + rand(n, 1)*(scale/2);
     a = rand(n, 1)*scale;
     b = rand(1, 1)*scale;
 end
