2b5d4b1c82b990934e8aef72d260cd75799202c9
Luca Moroni <terraformer.144@gmail.com>
2022-02-14 21:22:31 +0100

    documented svm part

--- src/KQPSVM.m
+++ src/KQPSVM.m
@@ -1,16 +1,15 @@
 classdef KQPSVM
-    % KQPSVM Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    % Class that implement the soft-margin SVM linear classifier
     properties
-        C
-        step_size
-        params_step_size
+        C % C parameter of the soft-margin
+        step_size % step function to be called from the quadratic programming solver
+        params_step_size % parameters passed to the step_size function
         
-        alpha_gez
-        d_svs
-        X_svs
-        b_o
+        % properties necessary to do prediction
+        alpha_svs % alpha element, lagrangian multiplier, associated to the support vectors
+        d_svs % target associated to the support vectors
+        X_svs % support vectors
+        b_o % interceptor of the classificator
     end
     
     methods
@@ -21,8 +20,17 @@ classdef KQPSVM
         end
         
         function obj = fit(obj, X, d)
-            % METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+            % fit method, create the classifier, based on the training
+            % data.
+            % Use the quadratic problem solved based on the gradient
+            % projection to calculate the lagrangian multiplier (alphas)
+            % necessary to compute the separator hyperplane
+            %
+            % input arguments:
+            % - X (matrix) the matrix of the training samples
+            % - d (vector) the vector of the training samples' target
+            % output arguments:
+            % - the model trained
             
             f = @(alpha) f_svm(alpha, d.*X);
 
@@ -37,11 +45,11 @@ classdef KQPSVM
             [alpha_star, ~, ~, ~, ~] = KQP(f, l, u, d, 0, x_start, 1e-15, 1e-15, 1000, obj.step_size, obj.params_step_size, 0, 1);
 
             svs_idx = alpha_star ~= 0;
-            obj.alpha_gez = alpha_star(svs_idx);
+            obj.alpha_svs = alpha_star(svs_idx);
             obj.d_svs = d(svs_idx);
             obj.X_svs = X(svs_idx, :);
 
-            on_margin_sv_idx = 0 < alpha_star & alpha_star < obj.C;
+            on_margin_sv_idx = 0 < obj.alpha_svs & obj.alpha_svs < obj.C;
             on_margin_sv = X(on_margin_sv_idx, :);
             on_margin_d = d(on_margin_sv_idx);
 
@@ -53,14 +61,21 @@ classdef KQPSVM
             for i= 1:N_prime
                 sv = on_margin_sv(i,:);
                 d_sv = on_margin_d(i);
-                obj.b_o = obj.b_o + (d_sv - (obj.alpha_gez'*(obj.d_svs.*obj.X_svs))*sv');
+                obj.b_o = obj.b_o + (d_sv - (obj.alpha_svs'*(obj.d_svs.*obj.X_svs))*sv');
             end
 
             obj.b_o = obj.b_o/N_prime;
         end
         
         function preds = predict(obj, X)
- 
+            % predict the X's rows target using the separator hyperplane
+            % trained by the fit call of this class.
+            %
+            % input arguments:
+            % - X (matrix) the matrix of the samples to predict the target
+            % output arguments:
+            % - preds (vector) the predicted targets of X's rows
+
             preds = sign((obj.alpha_gez'*(obj.d_svs.*obj.X_svs))*X' + obj.b_o);
             preds = preds';
         end
--- src/f_svm.m
+++ src/f_svm.m
@@ -1,5 +1,13 @@
 function [f, g] = f_svm(alpha, X)
-    % docstring
+    % objective function of the svm quadratic optimization problem
+    % input arguments:
+    % - X (matrix) the matrix of the training samples
+    % - alpha (vector) the lagrangian multiplier, element that has to be
+    % minimized
+    % output arguments:
+    % - f (scalar) the value of the function
+    % - g (vector) the vector of the gradient of the function
+    
     f = -(sum(alpha) - (1/2)*alpha'*X*X'*alpha);
     g = -(ones(size(alpha)) - X*X'*alpha);
 end
