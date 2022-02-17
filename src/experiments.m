278b8535b39a035938935f28e066b76aeddd26d4
Luca Moroni <terraformer.144@gmail.com>
2022-02-17 18:52:51 +0100

    added results and minor changes for large scale problems

--- src/experiments.m
+++ src/experiments.m
@@ -242,22 +242,22 @@ fprintf("relative error on the f reached fmincon step size, mean %d, var %d\n\n"
 % hold off
 
 figure('DefaultAxesFontSize',18);
-semilogy(f_s_kqp_mean_fs, 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_kqp_mean_fs), 'LineWidth',1);
 hold on
-semilogy(f_s_kqp_mean_diminishing, 'LineWidth',1);
-semilogy(f_s_kqp_mean_polyak, 'LineWidth',1);
-semilogy(f_s_kqp_mean_armijo_i, 'LineWidth',1);
-semilogy(f_s_kqp_mean_armijo_ii, 'LineWidth',1);
-semilogy(f_s_fmincon_mean, 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_kqp_mean_diminishing), 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_kqp_mean_polyak), 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_kqp_mean_armijo_i), 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_kqp_mean_armijo_ii), 'LineWidth',1);
+semilogy(remove_padding_from_sequence(f_s_fmincon_mean), 'LineWidth',1);
 lgd = legend('fixed','diminishing', 'polyak', 'armijo 1', 'armijo 2', 'fmincon');
 lgd.FontSize = 15;
 
 title('Relative norm of fs to f*', 'FontSize', 18);
+xlim([0, 501]);
 hold off
 
 
 function [seq] = padding_sequence(sequence, max_iter)
-    % docstring
     size_sequence = size(sequence);
     filled = size_sequence(2);
     
@@ -266,4 +266,10 @@ function [seq] = padding_sequence(sequence, max_iter)
     padding = repmat(sequence(:, end), 1, padding_size);
     
     seq = [sequence, padding];
+end
+
+function [seq] = remove_padding_from_sequence(sequence)
+    min_seq = min(sequence);
+    seq = sequence(sequence > min_seq + 1e-15);
+    seq = [seq, min_seq];
 end
--- src/large_scale_experiments.m
+++ src/large_scale_experiments.m
@@ -1,15 +1,17 @@
-n = 10000;
+n = 5000;
 scale = 10;
-n_samples = 10;
+n_samples = 1;
 inter_per  = 1;
 actv_per = -1;
 max_iters = 500;
 
 % fixed step size
 timing_kqp_fs = zeros(1, n_samples);
+relative_error_kqp_fs = zeros(1, n_samples);
 
 % polyak
 timing_kqp_polyak = zeros(1, n_samples);
+relative_error_kqp_polyak = zeros(1, n_samples);
 
 i = 0;
 
@@ -18,28 +20,34 @@ wait_bar = waitbar(0,'Processing your data');
 % bunch_cel is the name of the bunch of problems
 for problem_instance = 1:n_samples
     i = i+1;
-    
-    p = problem_instance{1};
-    
-    [Q, q, l, u, a, b, x_start] = generate_problem(n, scale, inter_per, actv_per);
         
-    eigs_Q = eig(Q);
+    [Q, q, l, u, a, b, x_start] = generate_problem(n, scale, inter_per, actv_per);
+    
+    tic;
+    [x_star, f_star] = minimize_matlab_kqp(x_start, Q, q, l, u, a, b, -1, true);
+    toc;
+    
+    tic;
+    eigs_Q = eigs(Q, 1);
+    tic;
+    
     L = max(eigs_Q);
-    tau = min(eigs_Q);
 
     f = @(x) objective_function(Q,q,x);
     
     % FIXED ---------
     
     tic;
-    [~, ~, x_s_kqp_fs, f_s_kqp_fs, g_s_kqp_fs] = KQP(f, l, u, a, b , x_start, 1e-6, 1e-15, max_iters, "fixed", 1/L, 0, 0);
+    [~, f_fixed, ~, ~, ~] = KQP(f, l, u, a, b , x_start, 1e-6, 1e-15, max_iters, "fixed", 1/L, 0, 0);
     timing_kqp_fs(i) = toc;
+    relative_error_kqp_fs(i) = abs(f_fixed - f_star)/abs(f_star);
     
     % POLYAK ---------
     
     tic;
-    [~, ~, x_s_kqp_polyak, f_s_kqp_polyak, g_s_kqp_polyak] = KQP(f, l, u, a, b , x_start, 1e-6, 1e-15, max_iters, "polyak", @(i) L^2/i, 0, 0);
+    [~, f_polyak, ~, ~, ~] = KQP(f, l, u, a, b , x_start, 1e-6, 1e-15, max_iters, "polyak", @(i) L^2/i, 0, 0);
     timing_kqp_polyak(i) = toc;
+    relative_error_kqp_polyak(i) = abs(f_polyak - f_star)/abs(f_star);
     
     % ---------
     
@@ -49,5 +57,7 @@ end
 % display the statistics over execution timing
 
 fprintf("convergence time fixed step size, mean %d, std %d\n", mean(timing_kqp_fs), std(timing_kqp_fs));
+fprintf("relative error fixed step size, mean %d, std %d\n", mean(relative_error_kqp_fs), std(relative_error_kqp_fs));
 
 fprintf("convergence time polyak step size, mean %d, std %d\n", mean(timing_kqp_polyak), std(timing_kqp_polyak));
+fprintf("relative error fixed step size, mean %d, std %d\n", mean(relative_error_kqp_polyak), std(relative_error_kqp_polyak));
